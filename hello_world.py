8680b1b1bc4683ab7bbd8246b45106c8a9c28af9
A-tomar <canuj.tomar@gmail.com>
2020-12-29 16:06:42 +0530

    Variables_Practice

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+name="anuj tomar"
+print("Name in title:")
+print(name.title())
+print("Name in lowercase:")
+print(name.lower())
+print("Name in uppercase:")
+print(name.upper())
+
+Fname="anuj"
+Lname="tomar"
+fullname=f"{Fname} {Lname}"
+print(f"Hello, {fullname.title()}!")
+
