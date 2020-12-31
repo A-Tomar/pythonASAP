24e3a715782b2bf3dbb75ba993cdbd15e8b00e04
canuj.tomar@gmail.com <canuj.tomar@gmail.com>
2020-12-31 13:15:21 +0530

    31-12-2020

--- hello_world.py
+++ hello_world.py
@@ -10,4 +10,18 @@ Fname="anuj"
 Lname="tomar"
 fullname=f"{Fname} {Lname}"
 print(f"Hello, {fullname.title()}!")
+print("Examples of new line & tabs")
+print("Cities: \n\tGwalior\n\tGreaterNoida\n\tDelhi\n\tPune")
 
+print("Stripping Whitespaces from strings")
+rname="Anuj "
+lname=" Anuj"
+bname=" Anuj "
+print(f"{rname}!")
+print(f"{rname.rstrip()}!")
+
+print(f"{lname}!")
+print(f"{lname.lstrip()}!")
+
+print(f"{bname}!")
+print(f"{bname.strip()}!")
